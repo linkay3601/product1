ca99efd4befd8ff2f3049a71cba16e9e1b59a9f0
Linkai <jink13467243601@gmail.com>
2020-03-22 18:17:56 +0800

    1. 完成了机器人初始化脚本 1. 完成了简易推荐算法

--- social/models.py
+++ social/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Swiped(models.Model):
+    '''滑动记录'''
+    FALGS = (
+        ('superlike', '上滑'),
+        ('like', '右滑'),
+        ('dislike', '左滑'),
+
+    )
+    uid = models.IntegerField(verbose_name='滑动者的 UID')
+    sid = models.IntegerField(verbose_name='被滑动者的 UID')
+    flag = models.CharField(max_length=10, choices=FALGS)
+    dtime = models.DateTimeField(auto_now=True)
+
+
+class Friend(models.Model):
+    '''好友关系'''
+    uid1 = models.IntegerField()
+    uid2 = models.IntegerField()
--- tiger/urls.py
+++ tiger/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 
 from user import api as user_api
+from social import api as social_api
 
 
 urlpatterns = [
@@ -24,4 +25,11 @@ urlpatterns = [
     url(r'^api/user/profile/show$', user_api.show_profile),
     url(r'^api/user/profile/modify$', user_api.modify_profile),
     url(r'^api/user/avatar/upload$', user_api.upload_avatar),
+
+    url(r'^api/social/rcmd_users$', social_api.get_rcmd_users),
+    url(r'^api/social/like$', social_api.like),
+    url(r'^api/social/superlike$', social_api.superlike),
+    url(r'^api/social/dislike$', social_api.dislike),
+    url(r'^api/social/rewind$', social_api.rewind),
+    url(r'^api/social/liked_me$', social_api.show_liked_me),
 ]
